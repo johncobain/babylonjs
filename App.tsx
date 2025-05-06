6048a5ae28179f19a18aa9f0b26512a092b13d12
Andrey Gomes <andreygdsn@gmail.com>
2025-05-06 15:03:12 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { EngineView, useEngine } from "@babylonjs/react-native";
+import {
+  ArcRotateCamera,
+  Camera,
+  Scene,
+  SceneLoader,
+  HemisphericLight,
+  Vector3,
+} from "@babylonjs/core";
+import "@babylonjs/loaders/glTF";
 
-export default function App() {
+function App(): React.JSX.Element {
+  const engine = useEngine();
+  const [scene, setScene] = useState<Scene>();
+  const [camera, setCamera] = useState<Camera>();
+
+  const basicGLTFURL =
+    "https://raw.githubusercontent.com/thechaudharysab/babylonjspoc/main/src/assets/Client.gltf";
+
+  //const horseGLTFURL = "https://raw.githubusercontent.com/thechaudharysab/babylonjspoc/main/src/assets/Horse.gltf";
+
+  const renderbasicGLTF = () => {
+    SceneLoader.LoadAsync(basicGLTFURL, undefined, engine)
+      .then((loadedScene) => {
+        if (loadedScene) {
+          setScene(loadedScene);
+
+          const light = new HemisphericLight(
+            "light",
+            new Vector3(0, 1, 0),
+            loadedScene
+          );
+          light.intensity = 0.7;
+
+          const camera = new ArcRotateCamera(
+            "camera",
+            -Math.PI / 2,
+            Math.PI / 2,
+            -6,
+            new Vector3(0, 2, 0),
+            loadedScene,
+            true
+          );
+          camera.attachControl(true);
+          setCamera(camera);
+        } else {
+          console.log("Error loading loadedScene.");
+        }
+      })
+      .catch((error) => {
+        console.log("Error loading scene: ", error);
+      });
+  };
+
+  useEffect(() => {
+    if (engine) {
+      renderbasicGLTF();
+    }
+  }, [engine]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <EngineView camera={camera} />
     </View>
   );
 }
@@ -13,8 +70,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
