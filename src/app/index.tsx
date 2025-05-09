2c5428f254e1fd64c7f0c9a482e9e885d1eda929
Andrey Gomes <andreygdsn@gmail.com>
2025-05-09 09:17:46 -0300

    horse rendering from link

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,8 +5,21 @@ import { router } from "expo-router";
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <Text>Home Page</Text>
-      <Text onPress={() => router.push("/render")}>Render</Text>
+      <Text style={{ fontSize: 32, alignSelf: "center", marginVertical: 24 }}>
+        Home Page
+      </Text>
+      <Text
+        style={{
+          padding: 16,
+          fontSize: 24,
+          borderColor: "#000",
+          borderWidth: 1,
+          backgroundColor: "#8686f6",
+        }}
+        onPress={() => router.push("/render")}
+      >
+        Render
+      </Text>
     </View>
   );
 }
--- src/app/render.tsx
+++ src/app/render.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
 import { EngineView, useEngine } from "@babylonjs/react-native";
 import {
   ArcRotateCamera,
   Camera,
   Scene,
   SceneLoader,
+  Color4,
+  AnimationGroup,
+  Nullable,
   HemisphericLight,
   Vector3,
 } from "@babylonjs/core";
@@ -15,6 +18,8 @@ function Render(): React.JSX.Element {
   const engine = useEngine();
   const [scene, setScene] = useState<Scene>();
   const [camera, setCamera] = useState<Camera>();
+  const [currentAnimation, setCurrentAnimation] =
+    useState<Nullable<AnimationGroup>>(null);
 
   // const basicGLTFURL = "https://raw.githubusercontent.com/thechaudharysab/babylonjspoc/main/src/assets/Client.gltf";
 
@@ -22,7 +27,7 @@ function Render(): React.JSX.Element {
     "https://raw.githubusercontent.com/thechaudharysab/babylonjspoc/main/src/assets/Horse.gltf";
 
   // const basicGLTFURL = require("../assets/Cow.glb");
-  // const basicGLTFURL = require("../assets/Horse.glb")
+  // const basicGLTFURL = require("../assets/Horse.glb");
   // const basicGLTFURL = require("../assets/Llama.glb")
   // const basicGLTFURL = require("../assets/Pig.glb")
   // const basicGLTFURL = require("../assets/Pug.glb")
@@ -30,7 +35,11 @@ function Render(): React.JSX.Element {
   // const basicGLTFURL = require("../assets/Zebra.glb")
 
   const renderbasicGLTF = () => {
-    SceneLoader.LoadAsync(basicGLTFURL, undefined, engine)
+    if (!engine) {
+      console.log("Engine not ready for loading GLTF");
+      return;
+    }
+    SceneLoader.LoadAsync("", basicGLTFURL, engine)
       .then((loadedScene) => {
         if (loadedScene) {
           setScene(loadedScene);
@@ -45,11 +54,19 @@ function Render(): React.JSX.Element {
             idleAnim.start(true, 1.0);
           }
         } else {
-          console.log("Error loading loadedScene.");
+          console.log("Error: SceneLoader.LoadAsync returned a null scene.");
         }
       })
       .catch((error) => {
         console.log("Error loading scene: ", error);
+        if (
+          error.message &&
+          error.message.includes("Unable to find a plugin")
+        ) {
+          console.error(
+            "This might mean the GLTF loader isn't registered or the file type is not recognized. Ensure '@babylonjs/loaders/glTF' is imported."
+          );
+        }
       });
   };
 
@@ -59,9 +76,87 @@ function Render(): React.JSX.Element {
     }
   }, [engine]);
 
+  const startWalkAnimation = () => {
+    if (scene) {
+      const walkAnimation = scene.getAnimationGroupByName("Walk");
+      if (walkAnimation) {
+        if (currentAnimation) {
+          currentAnimation.stop();
+        }
+        walkAnimation?.play(true);
+        setCurrentAnimation(walkAnimation);
+      } else {
+        console.warn("Animation not found:", walkAnimation);
+      }
+    }
+  };
+
+  const stopWalkAnimation = () => {
+    if (scene) {
+      const idleAnimation = scene.getAnimationGroupByName("Idle");
+      if (idleAnimation) {
+        if (currentAnimation) {
+          currentAnimation.stop();
+        }
+        idleAnimation.play(true);
+        setCurrentAnimation(idleAnimation);
+      } else {
+        console.warn("Animation not found:", idleAnimation);
+      }
+    }
+  };
+
+  const doJumpAnimation = () => {
+    if (scene) {
+      var jumpAnimation = scene.getAnimationGroupByName("Jump_toIdle");
+      if (jumpAnimation) {
+        jumpAnimation.play();
+      } else {
+        console.warn("Animation not found:", jumpAnimation);
+      }
+    }
+  };
+
+  const randomizeBGColor = () => {
+    if (scene) {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      const randomColor = new Color4(red / 255, green / 255, blue / 255, 1.0);
+
+      scene.clearColor = randomColor;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <EngineView camera={camera} />
+      <EngineView camera={camera} displayFrameRate={true} />
+      <View style={styles.absoluteView}>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={startWalkAnimation}
+        >
+          <Text>Start Walking</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={stopWalkAnimation}
+        >
+          <Text>Stop Walking</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={doJumpAnimation}
+        >
+          <Text>Jump</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={randomizeBGColor}
+        >
+          <Text>Random BG Color</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -70,6 +165,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  absoluteView: {
+    position: "absolute",
+    bottom: "10%",
+    width: "90%", // Give it some width
+    flexDirection: "row",
+    justifyContent: "space-around", // Distribute buttons
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#61dafb",
+    borderWidth: 1,
+    paddingVertical: 8, // Better padding
+    paddingHorizontal: 4,
+    marginHorizontal: 2, // Add some space between buttons
+  },
 });
-
 export default Render;
